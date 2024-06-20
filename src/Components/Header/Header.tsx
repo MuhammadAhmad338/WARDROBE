8d242e58eaa8ee42a42f4f8f06be6db3f025583c
Muhammad Ahmad <ahmadmuhammad.7700@gmail.com>
2024-06-20 05:50:07 -0700

    latest commits and latest commits

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import { RootState } from '../../Store/store';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import Upperheader from './UpperHeaders/upperheader';
 import Uppertolowerheader from './UpperHeaders/uppertolowerheader';
-import { Link } from 'react-router-dom';
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const isOpen = useSelector((state: RootState) => state.cart.isOpen);
+
   const cartItems = useSelector((state: RootState) => state.cart.items);
 
   const [isVisible, setIsVisible] = useState(true);
@@ -74,122 +75,117 @@ const Header = () => {
     }
   }, [lastScrollY]);
 
-  return (  
-    <>
-    <Upperheader />
-    <Uppertolowerheader />
-    <div className="relative bg-white p-4 shadow-lg">
-      <div className="flex items-center justify-between">
+  return (
+      <>
+        <Upperheader/>
+        <Uppertolowerheader/>
+
         {/* Left side: Navigation Links */}
-        <div className="flex items-center space-x-4 ">
-          <div className="text-black cursor-pointer lg:hidden" onClick={toggleDrawer}>
-            &#9776; Menu
-          </div>
-          <div className="hidden lg:flex space-x-4">
+        <header className="relative flex justify-between items-center px-6 py-6 bg-white shadow-md">
+          {/* Left: Navigation Links */}
+          <div className="flex space-x-4 custom-lg:flex hidden">
             <Link to="/" className="text-black text-sm hover:text-gray-500">Home</Link>
-             <Link to="/about" className="text-black text-sm hover:text-gray-500">About Us</Link>
-             <Link to="/contact" className="text-black text-sm hover:text-gray-500">Contact Us</Link>
-             <Link to="/products" className="text-black text-sm hover:text-gray-500">New Arrivals</Link>
-             <Link to="/products" className="text-black text-sm hover:text-gray-500">All Products</Link>
+            <Link to="/about" className="text-black text-sm hover:text-gray-500">About Us</Link>
+            <Link to="/contact" className="text-black text-sm hover:text-gray-500">Contact Us</Link>
+            <Link to="/allProducts" className="text-black text-sm hover:text-gray-500">New Arrivals</Link>
+            <Link to="/allProducts" className="text-black text-sm hover:text-gray-500">All Products</Link>
           </div>
-        </div>
-        
-        {/* Center: Logo */}
-       
-       {/* Center: Logo */}
-       <div className="text-black text-1xl lg:text-3xl lg:absolute lg:inset-x-0 lg:left-0 lg:right-0 lg:text-center">
-          <Link to="/">
+
+          {/* Center: Logo */}
+          <div
+              className="absolute left-1/2 transform -translate-x-1/2 text-xl sm:text-2xl lg:text-3xl text-black">
             W A R D R O B E
-          </Link>
-        </div>
-        
-        {/* Right side: Cart and Search */}
-        <div className="flex items-center space-x-4">
-          {/* Cart Icon */}
-          <div className="text-black cursor-pointer" onClick={toggleCart}>
-            &#128722; 
           </div>
-          
-          {/* Search Icon */}
-          <div className="text-black cursor-pointer" onClick={toggleSearch}>
-            &#128269; 
+          <div className="custom-lg:hidden text-black cursor-pointer" onClick={toggleDrawer}>
+            <FontAwesomeIcon icon={faBars} />
           </div>
-        </div>
-      </div>
 
-      {/* Drawer */}
-      {isDrawerOpen && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50 lg:hidden">
-          <div className="absolute top-0 left-0 h-full w-4/5 sm:w-80 lg:w-60 bg-white shadow-lg">
-            <div className="p-4">
-              {/* Drawer Content with Links */}
-              <div className="flex flex-col space-y-4">
-                <Link to="/" className="text-black hover:text-gray-500" onClick={toggleDrawer}>Home</Link>
-                <Link to="/about" className="text-black hover:text-gray-500" onClick={toggleDrawer}>About</Link>
-                <Link to="/contact" className="text-black hover:text-gray-500" onClick={toggleDrawer}>Contact</Link>
-                <Link to="/products" className="text-black hover:text-gray-500" onClick={toggleDrawer}>All Products</Link>
-              </div>
-              <button className="text-gray-800 mt-4" onClick={toggleDrawer}>Close</button>
-            </div>
-          </div>
-        </div>
-      )}
-       {/* Cart */}
-     
-      {/* Cart */}
-      {isCartOpen && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50">
-          <div className="absolute top-0 right-0 h-full w-4/5 sm:w-80 lg:w-60 bg-white shadow-lg rounded-lg">
-            <div className="p-4 flex flex-col h-full">
-              <h2 className="text-lg font-semibold">Shopping Cart</h2>
-              <div className="overflow-y-auto flex-1">
-                {items.map(item => (
-                  <div key={item.id} className="flex items-center space-x-4 mt-4">
-                    <img src={item.image} alt={item.title} className="w-16 h-16 object-cover rounded" />
-                    <div className="flex-1">
-                      <h3 className="text-sm font-medium">{item.title}</h3>
-                      <p className="text-gray-600">${item.price.toFixed(2)}</p>
-                      <div className="flex items-center mt-1">
-                        <button onClick={() => decreaseQuantity(item.id)} className="text-gray-500 hover:text-gray-700 px-2">
-                          -
-                        </button>
-                        <span className="px-2">{item.quantity}</span>
-                        <button onClick={() => increaseQuantity(item.id)} className="text-gray-500 hover:text-gray-700 px-2">
-                          +
-                        </button>
-                      </div>
-                    </div>
-                    <button onClick={() => removeItem(item.id)} className="text-gray-500 hover:text-gray-700">
-                      Remove
-                    </button>
+          {/* Drawer */}
+          {isDrawerOpen && (
+              <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50 lg:hidden">
+                <div className="absolute top-0 left-0 h-full w-4/5 sm:w-82 bg-white shadow-lg rounded-lg p-4">
+                  <button className="text-gray-800 mb-4" onClick={toggleDrawer}>X</button>
+                  <div className="flex flex-col space-y-4">
+                    <Link to="/" className="text-black hover:text-gray-500" onClick={toggleDrawer}>Home</Link>
+                    <Link to="/about" className="text-black hover:text-gray-500" onClick={toggleDrawer}>About Us</Link>
+                    <Link to="/contact" className="text-black hover:text-gray-500" onClick={toggleDrawer}>Contact
+                      Us</Link>
+                    <Link to="/allProducts" className="text-black hover:text-gray-500" onClick={toggleDrawer}>New
+                      Arrivals</Link>
+                    <Link to="/allProducts" className="text-black hover:text-gray-500" onClick={toggleDrawer}>All
+                      Products</Link>
                   </div>
-                ))}
+                </div>
               </div>
-              <div className="mt-6 flex justify-between items-center">
-                <p className="text-xl font-semibold">Total:</p>
-                <p className="text-xl">${totalPrice.toFixed(2)}</p>
-              </div>
-              <button className="mt-6 w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg shadow-sm">Proceed to Checkout</button>
-              <button className="text-gray-800 mt-4" onClick={toggleCart}>Close</button>
-            </div>
-          </div>
-        </div>
-      )}
+          )}
+
+          <Link to="/cart" className="text-black text-sm hover:text-gray-500">Cart</Link>
+        </header>
 
 
-      {/* Search */}
-      {isSearchOpen && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50">
-          <div className="absolute top-0 right-0 h-full w-4/5 sm:w-80 lg:w-60 bg-white shadow-lg">
-            <div className="p-4">
-              Search Content
-              <button className="text-gray-800" onClick={toggleSearch}>Close</button>
+        {/* Center: Logo */}
+
+        {/* Center: Logo */}
+
+
+        {/* Cart */}
+        {isCartOpen && (
+            <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50">
+              <div className="absolute top-0 right-0 h-full w-4/5 sm:w-80 lg:w-60 bg-white shadow-lg rounded-lg">
+                <div className="p-4 flex flex-col h-full">
+                  <h2 className="text-lg font-semibold">Shopping Cart</h2>
+                  <div className="overflow-y-auto flex-1">
+                    {items.map(item => (
+                        <div key={item.id} className="flex items-center space-x-4 mt-4">
+                          <img src={item.image} alt={item.title} className="w-16 h-16 object-cover rounded"/>
+                          <div className="flex-1">
+                            <h3 className="text-sm font-medium">{item.title}</h3>
+                            <p className="text-gray-600">${item.price.toFixed(2)}</p>
+                            <div className="flex items-center mt-1">
+                              <button onClick={() => decreaseQuantity(item.id)}
+                                      className="text-gray-500 hover:text-gray-700 px-2">
+                                -
+                              </button>
+                              <span className="px-2">{item.quantity}</span>
+                              <button onClick={() => increaseQuantity(item.id)}
+                                      className="text-gray-500 hover:text-gray-700 px-2">
+                                +
+                              </button>
+                            </div>
+                          </div>
+                          <button onClick={() => removeItem(item.id)} className="text-gray-500 hover:text-gray-700">
+                            Remove
+                          </button>
+                        </div>
+                    ))}
+                  </div>
+                  <div className="mt-6 flex justify-between items-center">
+                    <p className="text-xl font-semibold">Total:</p>
+                    <p className="text-xl">${totalPrice.toFixed(2)}</p>
+                  </div>
+                  <button
+                      className="mt-6 w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg shadow-sm">Proceed
+                    to Checkout
+                  </button>
+                  <button className="text-gray-800 mt-4" onClick={toggleCart}>Close</button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
-    </div>
-   </>
+        )}
+
+
+        {/* Search */}
+        {isSearchOpen && (
+            <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50">
+              <div className="absolute top-0 right-0 h-full w-4/5 sm:w-80 lg:w-60 bg-white shadow-lg">
+                <div className="p-4">
+                  Search Content
+                  <button className="text-gray-800" onClick={toggleSearch}>Close</button>
+                </div>
+              </div>
+            </div>
+        )}
+      </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
       fontFamily: {
         body: ["Montserrat"]
       },
+      screens: {
+        'custom-lg': '1132px',
+      },
       colors: {
         'peach': '#FFDAB9', // Adjust the hex code to match your desired peach shade,
         'peach-lightest': '#FFF4E5', // Adjusted to a lighter shade of peach
