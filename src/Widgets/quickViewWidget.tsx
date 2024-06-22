6f490b95b7f87a1a1e6a80443b5f09e81c2ebd5c
Muhammad Ahmad <ahmadmuhammad.7700@gmail.com>
2024-06-22 05:49:24 -0700

    latest commits and super softwares

--- src/Widgets/quickViewWidget.tsx
+++ src/Widgets/quickViewWidget.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 
 interface QuickViewWidgetProps {
   name: string;
@@ -8,39 +8,59 @@ interface QuickViewWidgetProps {
   handleQuickViewToggle: () => void;
 }
 
-const QuickViewWidget: React.FC<QuickViewWidgetProps> = ({ name, image, description, price, handleQuickViewToggle }) => {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4 sm:p-6 md:p-8">
-  <div className="bg-white max-w-3xl w-full h-auto md:h-3/4 lg:h-3/5 shadow-lg flex flex-col md:flex-row relative">
-    <div className="relative w-full md:w-1/2 flex items-center justify-center">
-      <div className="w-full h-[300px] max-w-md">
-        <img className="w-full h-full bject-cover" src={image} alt={name} />
-      </div>
-    </div>
-    <div className="w-full md:w-1/2 p-4 sm:p-8 flex flex-col justify-between relative">
-      <button
-        className="absolute top-2 right-2 sm:top-4 sm:right-4 bg-black text-white py-2 px-3 sm:py-3 sm:px-4 hover:bg-gray-800 transition duration-300"
-        onClick={handleQuickViewToggle}
-      >
-        ✕
-      </button>
-      <div>
-        <h2 className="text-xl sm:text-2xl mb-2 sm:mb-4 text-gray-800">{name}</h2>
-        <p className="text-gray-700 text-xs sm:text-sm mb-4 sm:mb-8">{description}</p>
-      </div>
-      <div className="flex justify-between items-center mt-4 sm:mt-0">
-        <span className="text-lg sm:text-xl text-gray-800">$ {price.toFixed(2)}</span>
-        <button className="bg-black text-white hover:bg-white hover:border hover:border-black hover:text-black font-bold py-2 px-4 focus:outline-none focus:shadow-outline transition duration-300">
-          Add to Cart
-        </button>
-      </div>
-    </div>
-  </div>
-</div>
+const QuickViewWidget: React.FC<QuickViewWidgetProps> = ({
+  image,
+  name,
+  description,
+  price,
+  handleQuickViewToggle
+}) => {
+  const [selectedSize, setSelectedSize] = useState("S");
 
+  const handleSizeClick = (size: string) => {
+    setSelectedSize(size);
+  };
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4 sm:p-6 md:p-8 ">
+      <div className="flex justify-start flex-col space-y-4 bg-white md:flex md:flex-row lg:flex lg:flex-row mt-12 mb-12">
+        <img src={image} className="h-[300px] " alt="" />      
+        <div className="p-6">
+          <div className="flex flex-row justify-between">
+          <p>{name}</p>
+           <div onClick={handleQuickViewToggle} className="border bg-white text-black hover:bg-black hover:text-white px-3 py-1 hover:cursor-pointer border-black">X</div>
+          </div>
+          <p>{price}</p>
+          <p>{description}</p>
+          <p>Size: s</p>
+          <div className="flex mt-3 space-x-4 ">
+            {["S", "M", "L", "XL"].map((size) => (
+              <>
+                <div
+                  key={size}
+                  onClick={() => handleSizeClick(size)}
+                  className={`  px-3 py-1 border-2 cursor-pointer transition-colors ${
+                    selectedSize === size
+                      ? "bg-black text-white border-black"
+                      : "bg-white text-black border-gray-300"
+                  } hover:bg-black hover:text-white`}
+                >
+                  <span className="text-lg">{size}</span>
+                </div>
+              </>
+            ))}
+          </div>
+          <button
+            className="bg-white text-black border 
+      px-4 py-2 w-full mt-4
+      border:black hover:bg-black hover:text-white hover:border hover:border-white"
+            onClick={() => {}}
+          >
+            Add To Cart
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default QuickViewWidget;
-
-
